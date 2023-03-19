7a3e104fd274e53e31bf56009652a45b07cdd501
marciomizuhara <marciomizu@gmail.com>
2023-03-19 14:54:50 -0300

    Added option to download videos too

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ chan_thread = input('Please insert the url of the fapello profile you want to sc
 
 # Input user for a folder path to save the media
 
-folder_path = input('Please insert a folder path to save the media: ')
-# folder_path = 'N:\TORRENTS\System\_fapello\\'
+# folder_path = input('Please insert a folder path to save the media: ')
+folder_path = 'N:\TORRENTS\System\_fapello\\'
 folder_path.replace("\\","/")
+download_video = input('Do you want to try downloading videos too? Type Y or N: ')
 
 
 # Set up requests
@@ -71,37 +72,91 @@ avatar = avatars_list[0]['src']
 profile_name = chan_thread.split('.com/')[-1]
 mkdir_p(f'{folder_path}/{profile_name}')
 
+video_pages = []
+
 base_url = chan_thread
 counter = 1
 
 for image in range(pages_amount):
     try:
         my_url = Request(f"{base_url}{counter}", headers={'User-Agent': 'Mozilla/5.0'})
-        print(f'Analyzing page {base_url}{counter} ...')
+        print(f'Analyzing images in page {base_url}{counter} ...')
         uClient = uReq(my_url)
-        time.sleep(0.5)
+        time.sleep(0.1)
         page_html = uClient.read()
-        time.sleep(0.5)
+        time.sleep(0.1)
         uClient.close()
-
-
         # html parsing
         page_soup = soup(page_html, "html.parser")
-
+        videos = page_soup.findAll('video')
         extended_images = page_soup.findAll('img')
-        # input('new')
-        for image in extended_images:
-            if profile_name in image['src'] and avatar not in image['src']:
-                # extended_image_list.append(image['src'])
-                non_formatted = image['src'].split('/')[-1]
-                webm_link = image['src']
-                webm_data = requests.get(f'{webm_link}').content
-                with open(f'{folder_path}/{profile_name}/{non_formatted}','wb') as handler:
-                    handler.write(webm_data)
-                print(f'Image {image["src"]} downloaded successfully!')
-            else:
+
+        if videos:
+            video_pages.append(counter)
+            counter += 1
+        else:
+            try:
+                for image in extended_images:
+                    if profile_name in image['src'] and avatar not in image['src']:
+                        # extended_image_list.append(image['src'])
+                        non_formatted = image['src'].split('/')[-1]
+                        webm_link = image['src']
+                        webm_data = requests.get(f'{webm_link}').content
+                        with open(f'{folder_path}/{profile_name}/{non_formatted}','wb') as handler:
+                            handler.write(webm_data)
+                        print(f'Image {image["src"]} downloaded successfully!')
+                    else:
+                        continue
+            except:
                 continue
-                #print(f'Error downloading {image["src"]}')
-        counter += 1
+            counter += 1
     except:
-        print(f'Total of {counter} images downloaded successfully!')
+        pass
+
+print(f'Total of {counter} images downloaded successfully!')
+
+counter = 1
+# VIDEO
+if download_video.lower() == 'y':
+    for page in video_pages:
+        try:
+            my_url = Request(f"{base_url}{page}", headers={'User-Agent': 'Mozilla/5.0'})
+            print(f'Analyzing videos in page {base_url}{page} ...')
+            uClient = uReq(my_url)
+            time.sleep(0.1)
+            page_html = uClient.read()
+            time.sleep(0.1)
+            uClient.close()
+            # html parsing
+            page_soup = soup(page_html, "html.parser")
+            videos = page_soup.findAll('video')
+            try:
+                # print('entrou 0')
+                for _video in videos:
+                    # print('entrou 0.5')
+                    for video in _video:
+                        try:
+                            # extended_image_list.append(image['src'])
+                            non_formatted = video['src'].split('/')[-1]
+                            webm_link = video['src']
+                            webm_data = requests.get(f'{webm_link}').content
+
+                            with open(f'{folder_path}/{profile_name}/{non_formatted}', 'wb') as handler:
+                                handler.write(webm_data)
+                            print(f'Video {video["src"]} downloaded successfully!')
+                            continue
+                        except:
+                            pass
+                            # counter += 1
+                # counter += 1
+            except:
+                pass
+                # print('entrou aqui 1')
+                # counter += 1
+
+        except:
+            continue
+            # counter += 1
+print(f'Total of {len(video_pages)} videos downloaded successfully!')
+
+
